696af12f7cbb15126ce958315a9ad1330611f54d
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-01-14 23:44:26 +0100

    Added implementation of single column of MDNN architecture.

--- traffic_sign_classifier2.py
+++ traffic_sign_classifier2.py
@@ -91,22 +91,45 @@ y_test = to_categorical(y_test, n_classes)
 
 network = tflearn.input_data(shape=[None, image_shape, image_shape, 3])
 
-
+# First layer is a convolution with filter size 32 and relu activation.
 network = tflearn.conv_2d(network, 32, 3, activation='relu')
+# Second layer is convolution with filter size of 7 and 100 depth.
+network = tflearn.conv_2d(network, 100, 7, activation='relu')
+# Maxpool with stride 2.
 network = tflearn.max_pool_2d(network, 2)
-network = tflearn.conv_2d(network, 64, 3, activation='relu')
-network = tflearn.conv_2d(network, 64, 3, activation='relu')
+# Fourth layer is convolution with filter size 4 and 150 depth and relu activation.
+network = tflearn.conv_2d(network, 150, 4, activation='relu')
+# Fifth layer is max pool with stride 2.
 network = tflearn.max_pool_2d(network, 2)
-network = tflearn.fully_connected(network, 512, activation='relu')
+# Sixth layer is convolution with filter size 4 and 250 depth and relu activation.
+network = tflearn.conv_2d(network, 250, 4, activation='relu')
+# Seventh layer is max pool with stride 2.
+network = tflearn.max_pool_2d(network, 2)
+# We include dropout layer with 50% drop-out probability.
 network = tflearn.dropout(network, 0.5)
+# Ninth layer is fully connected.
+network = tflearn.fully_connected(network, 300, activation='relu')
+# Fully connected layer with output size equal to 43, which is the number
+# of classes we train on and softmax activation to increase the distance
+# between the logit scores.
 network = tflearn.fully_connected(network, n_classes, activation='softmax')
+
+# Regression layer that will help us tune the neural network.
+# Explanation of the parameter selection:
+# * As an optimizer we select 'Adaptive Moment Estimation' (see https://arxiv.org/pdf/1412.6980.pdf)
+#   this method has low memory requirements and only requires the first order derivatives
+#   of the tensors. The latter requirement results in faster computations.
+# * as learning_rate we set a low value to prevent the stochastic gradient descent algorithm
+#   to jump too large distances at once.
+# * as loss function we choose categorical_crossentropy since we seek to
+#   categorize images. The categorical cross entropy function will compute errors
+#   in the categorization task.
 network = tflearn.regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
                      learning_rate=0.001)
-
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=2,tensorboard_dir='/tmp/tflearn_logs/')
 
 model.fit(X_input, y_train, n_epoch=50, shuffle=True,
           validation_set=(X_test_norm, y_test),
-          show_metric=True, batch_size=240, run_id='traffic_cnn')
+          show_metric=True, batch_size=240, run_id='traffic_cnn2')
