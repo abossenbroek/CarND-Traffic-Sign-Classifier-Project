cbce3d076ef8b109097b3bbd3d35f2a20405a27c
Anton Bossenbroek <anton.bossenbroek@me.coml>
2017-01-15 18:36:39 +0000

    Added safe of model.

--- traffic_sign_classifier2.py
+++ traffic_sign_classifier2.py
@@ -130,7 +130,6 @@ network = tflearn.regression(network, optimizer='adam',
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=2,tensorboard_dir='/tmp/tflearn_logs/')
 
-print(X_input.shape)
-#model.fit(X_input, y_train, n_epoch=50, shuffle=True,
-          #validation_set=(X_test_norm, y_test),
-          #show_metric=True, batch_size=240, run_id='traffic_cnn2')
+model.fit(X_input, y_train, n_epoch=50, shuffle=True,
+          validation_set=(X_test_norm, y_test),
+          show_metric=True, batch_size=240, run_id='traffic_cnn2')
